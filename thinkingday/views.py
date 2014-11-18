cef518535b53b98f47abad86b4610cd3a2735490
Oriole <oriole@vieux-mazel.ch>
2014-11-18 09:59:10 +0100

    Redirect to scouts.ch for now

--- thinkingday/views.py
+++ thinkingday/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render  # , redirect
-#from django.http import Http404
+# from django.shortcuts import render  # , redirect
+from django.http import HttpResponseRedirect
 
 
 def home(request):
-    return render(request, 'home.html')
+    # Redirect to scout.ch temporarily
+    return HttpResponseRedirect('http://www.scouts.ch/')
+    # return render(request, 'home.html')
