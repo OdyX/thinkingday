ce039a73497e1de1e432975643a6096a4b4b5a72
Oriole <oriole@vieux-mazel.ch>
2014-12-11 21:37:38 +0100

    Affiche la description du prochain événement sur la page d'accueil

--- thinkingday/views.py
+++ thinkingday/views.py
@@ -2,19 +2,15 @@
 from django.shortcuts import render  # , redirect
 from django.contrib.auth.models import User
 from apps.user.forms import RegisterForm
+from apps.event.models import Event
 
 
 def home(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = User()
-            user.email = form.cleaned_data['email']
-            #user.username = form.cleaned_data['email']
-            user.save()
-    else:
-        form = RegisterForm()
+    try:
+        event = Event.objects.filter().order_by('start')[0]
+    except:
+        event = None
 
     return render(request, 'home.html', {
-            'form': form
+            'event': event
             })
