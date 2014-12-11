b10e61f29cd67249ce6c95546d576cb117064d47
Oriole <oriole@vieux-mazel.ch>
2014-12-11 22:16:29 +0100

    Implémente le fallback correctement

--- thinkingday/views.py
+++ thinkingday/views.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render  # , redirect
-from django.contrib.auth.models import User
-from apps.user.forms import RegisterForm
 from apps.event.models import Event
-from django.conf import settings
 
 
 def home(request):
     try:
-        event = Event.objects.untranslated()\
-        .use_fallbacks(settings.LANGUAGE_CODE).order_by('start')[0]
+        event = Event.objects\
+            .untranslated()\
+            .use_fallbacks()\
+            .order_by('start')[0]
     except:
         event = None
 
     return render(request, 'home.html', {
-            'event': event
-            })
+                  'event': event
+                  })
