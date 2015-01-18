850f7bbf9f76d1c594986b45d0488cb411a21b13
Pygargue <pygargue@lac-bleu.ch>
2015-01-18 15:22:16 +0100

    Préparation de la requête AJAX sur les messages postés -> affichage sur la carte.

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = patterns('',
     url(r'^$', 'apps.event.views.map', name='map'),
+    url(r'^messages$', 'apps.event.query.messages', name='messages'),
 )
