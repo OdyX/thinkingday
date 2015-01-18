8ceade2263c95817256d17cbed2b55cadb9b1502
Pygargue <pygargue@lac-bleu.ch>
2015-01-18 15:01:56 +0100

    Récupération des events dans la vue map

--- apps/event/views.py
+++ apps/event/views.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render  # , redirect
+from apps.event.models import Event
 
 def map(request):
-    return render(request, 'map.html')
+    try:
+        event = Event.objects\
+            .untranslated()\
+            .use_fallbacks()\
+            .order_by('start')[0]
+    except:
+        event = None
+
+    return render(request, 'map.html', {
+            'event': event,
+            })
