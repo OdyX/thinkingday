aad8bbe7ef91fe3123c130a1fa21284f581913fa
Oriole <oriole@vieux-mazel.ch>
2015-01-20 15:42:48 +0100

    More fixes

--- apps/event/views.py
+++ apps/event/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from apps.event.models import get_event_by_codename
 
 
-def map(request, event_codename):
+def map(request, event_codename=None):
     return render(request, 'map.html', {
             'event': get_event_by_codename(event_codename),
             })
 
 
-def messages(request, event_codename):
+def messages(request, event_codename=None):
     return render(request, 'map.html', {
             'event': get_event_by_codename(event_codename),
             })  # change to respond messages
