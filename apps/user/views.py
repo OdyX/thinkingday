0d3318c7363895287378096b177db8f0965a7920
Oriole <oriole@vieux-mazel.ch>
2015-01-20 13:09:53 +0100

    Correct the social account stuff

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render  # , redirect
 from django.contrib.auth.decorators import login_required
+from allauth.socialaccount.models import SocialAccount
 
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    socaccounts = SocialAccount.objects.filter(user_id=request.user.id)
+    return render(request, 'profile.html', {'socaccounts': socaccounts})
