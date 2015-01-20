6475ad9b7f05896de342e656de1f1e18b909c6ed
Oriole <oriole@vieux-mazel.ch>
2015-01-20 13:10:21 +0100

    Add the userprofile migrations

---
+++ apps/user/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('socialaccount', '0002_auto_20150120_1126'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('scoutname', models.CharField(max_length=512, null=True, blank=True)),
+                ('socialaccount', models.ForeignKey(related_name='profile', null=True, blank=True, to='socialaccount.SocialAccount', unique=True)),
+                ('user', models.OneToOneField(related_name='profile', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'user_profile',
+            },
+            bases=(models.Model,),
+        ),
+    ]
