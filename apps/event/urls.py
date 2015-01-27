fdca150ad4ff963a7f7a2871fd176df486dcb095
Pygargue <pygargue@lac-bleu.ch>
2015-01-27 09:52:43 +0100

    Correction de pattern d'URL

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'apps.event.views.map', name='map'),
     url(r'^points.json$', 'apps.event.views.points', name='points'),
-    url(r'^(?P<point_id>\d+)?/messages.json$', 'apps.event.views.messages', name='messages'),
+    url(r'^(?P<point_id>\w+)?/messages.json$', 'apps.event.views.messages', name='messages'),
 )
