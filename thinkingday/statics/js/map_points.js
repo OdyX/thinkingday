4c707e387015de433f034aeb84bf3a706d2b468d
Pygargue <pygargue@lac-bleu.ch>
2015-01-27 14:03:50 +0100

    Limite l'affichage de la carte

--- thinkingday/statics/js/map_points.js
+++ thinkingday/statics/js/map_points.js
@@ -61,8 +61,7 @@ var map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
-            source: new ol.source.OSM(),
-            extent: ol.proj.transformExtent([3, 45, 15, 49], 'EPSG:4326', 'EPSG:900913')
+            source: new ol.source.OSM()
         }),
         messagesLayer,
         tempLayer
